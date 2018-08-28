97172de68673bf5d525cd8bfd5e3085637fe1e88
Gio <liu.gio@lexisnexis.com>
2018-08-28 10:32:37 +0800

    clear code

--- exhibition/resources/views/wechat/life/detail.blade.php
+++ exhibition/resources/views/wechat/life/detail.blade.php
@@ -7,17 +7,17 @@
     <link rel="stylesheet" href="{{ asset('public/css/css.css') }}" />
     <title>大金空调</title>
     <style>
-    	
-	.video{ overflow:hidden;  border-top:4px solid #52c3f1; text-align:center; font-size:16px; color:#666;}
-	.video video{ width:640px; height:326px;}
-	
-	.video-txt{ padding:40px; border-bottom:1px solid #ccc;}
-	.video-txt b{ margin-bottom:10px; display:block; font-size:24px;}
-	.video-txt p{ font-size:20px; color:#666; line-height:32px;}
-	
-	.designerd-div{ padding:20px 40px;}
-	.designerd-div .title{ margin-bottom:20px;}
-	.designerd-div .title span{ margin-right:15px; padding:0 10px; display:inline-block; color:#fff; background:#ff6700; font-size:18px; height:36px; line-height:36px;}
+        
+    .video{ overflow:hidden;  border-top:4px solid #52c3f1; text-align:center; font-size:16px; color:#666;}
+    .video video{ width:640px; height:326px;}
+    
+    .video-txt{ padding:40px; border-bottom:1px solid #ccc;}
+    .video-txt b{ margin-bottom:10px; display:block; font-size:24px;}
+    .video-txt p{ font-size:20px; color:#666; line-height:32px;}
+    
+    .designerd-div{ padding:20px 40px;}
+    .designerd-div .title{ margin-bottom:20px;}
+    .designerd-div .title span{ margin-right:15px; padding:0 10px; display:inline-block; color:#fff; background:#ff6700; font-size:18px; height:36px; line-height:36px;}
     .designerd-div .col{ margin-bottom:20px; overflow:hidden;}
     .designerd-div .ovf{ margin-right:15px; float:left; width:202px;}
     .designerd-div .txt{}
@@ -26,76 +26,75 @@
     
     .other-div{ padding:20px 40px;}
     .other-div .title{ margin-bottom:20px;}
-	.other-div .title span{ margin-right:15px; padding:0 10px; display:inline-block; color:#fff; background:#0096e0; font-size:18px; height:36px; line-height:36px;}
-   	
-   	.other-div .col{ margin-bottom:20px; overflow:hidden;}
+    .other-div .title span{ margin-right:15px; padding:0 10px; display:inline-block; color:#fff; background:#0096e0; font-size:18px; height:36px; line-height:36px;}
+       
+       .other-div .col{ margin-bottom:20px; overflow:hidden;}
     .other-div .ovf{ margin-right:15px; float:left; width:230px;}
     .other-div .txt b{ font-size:24px; margin-bottom:20px; display:block; margin-bottom:10px;}
-    .other-div .txt p{ height:60px; overflow:hidden; font-size:18px; color:#666; line-height:32px;}
+    .other-div .txt p{ overflow:hidden; font-size:18px; color:#666; line-height:32px;}
     
-   	
+       
     
     </style>
 </head>
 <body>
 
 <div class="container">
-	
-	<div class="head">
-		<a href="javascript:void(0);" class="logo"></a>
-	</div>
-	
-	<div class="video">
-		<video src=""></video>
+    
+    <div class="head">
+        <a href="javascript:void(0);" class="logo"></a>
+    </div>
+    
+    <div class="video">
         {!! $data->video !!}
-		<label>(请在wifi情况下观看）</label>
-	</div>
-	
-	<div class="video-txt">
-		<b>{{ $data->title }}</b>
-		<p>{!! $data->desc !!}</p>
-	</div>
-	
-	<!--设计师-->
+        <label>(请在wifi情况下观看）</label>
+    </div>
+    
+    <div class="video-txt">
+        <b>{{ $data->title }}</b>
+        <p>{!! $data->desc !!}</p>
+    </div>
+    
+    <!--设计师-->
     @if (!empty($designer))
-	<div class="designerd-div ">
-		<div class="title"><span>设计师</span><label>{{ $designer->name }}</label></div>
-		<div class="designer-list">
-			<div class="col">
+    <div class="designerd-div ">
+        <div class="title"><span>设计师</span><label>{{ $designer->name }}</label></div>
+        <div class="designer-list">
+            <div class="col">
                 @if (!empty($designer->img))
-				<div class="ovf"><img src="{{ $designer->img }}" /></div>
+                <div class="ovf"><img src="{{ $designer->img }}" /></div>
                 @endif
-				<div class="txt">
-					<b>{{ $designer->title }}</b>
-					{!! $designer->desc !!}
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="txt">
+                    <b>{{ $designer->title }}</b>
+                    {!! $designer->desc !!}
+                </div>
+            </div>
+        </div>
+    </div>
     @endif
-	<!--设计师-->
-	
+    <!--设计师-->
+    
     @if (!empty($recommend))
-	<!--其他优秀设计-->
-	<div class="other-div">
-		<div class="title"><span>其他优秀设计</span></div>
-		<div class="other-list">
+    <!--其他优秀设计-->
+    <div class="other-div">
+        <div class="title"><span>其他优秀设计</span></div>
+        <div class="other-list">
         @foreach($recommend as $life)
-			<div class="col">
+            <div class="col">
                 @if (!empty($life->img))
-				<div class="ovf"><img src="{{ $life->img }}"></div>
+                <div class="ovf"><img src="{{ $life->img }}"></div>
                 @endif
-				<div class="txt">
-					<b>{{ $life->title }}</b>
-					<p>{!! $life->desc !!}</p>
-				</div>
-			</div>  
+                <div class="txt">
+                    <b>{{ $life->title }}</b>
+                    <p>{!! $life->desc !!}</p>
+                </div>
+            </div>  
         @endforeach
-		</div>
-	</div>
-	<!--其他优秀设计-->
+        </div>
+    </div>
+    <!--其他优秀设计-->
     @endif
-	
+    
 </div>
 
 </body>
